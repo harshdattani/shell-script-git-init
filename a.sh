b6687eae60ff592a0aef3f4a9bd9d34f17d5b4df
harshdattani <harsh.dattani@hotmail.com>
2014-12-01 22:04:07 +0530

    My first commit

---
+++ a.sh
@@ -0,0 +1,10 @@
+# Made By Harsh Dattani
+git init
+git status
+git add *
+git commit -m "My first commit" 
+git status
+print -n Enter remote URL
+read url
+git remote add origin $url
+git push -u origin master
